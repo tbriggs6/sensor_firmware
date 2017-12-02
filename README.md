50f9e16c01267fadcc37fbae1c213167e15fbf69
Tom Briggs <tbriggs@cs.ship.edu>
2017-12-02 14:59:54 -0800

    Created the initial pass of the message framework
    I went to test this and the broadcast problem came back...

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 CC1310 Contiki Firmware for Water Sensors
+
+issue 3 - add a communications layer for the sensors
--- testSensor/testsensor.c
+++ testSensor/testsensor.c
@@ -11,10 +11,30 @@
 #include <net/ip/uip-debug.h>
 #include <net/ipv6/uip-ds6.h>
 
+#include <message-service.h>
+
 PROCESS(test_bcast_cb, "Test Sensor Broadcast Handler");
 
 AUTOSTART_PROCESSES(&test_bcast_cb);
 
+#define ECHO_REQ 0x323232
+#define ECHO_REPL 0x232323
+typedef struct {
+	uint32_t header;
+	char message[32];
+} echo_t;
+
+void echo_handler(uip_ipaddr_t *remote_addr, int remote_port, char *data, int length)
+{
+	echo_t *echoreq = (echo_t *) data;
+	if (length != sizeof(echo_t)) return;
+
+	echo_t echo_rep;
+	memcpy(&echo_rep, echoreq, sizeof(echo_t));
+	echo_rep.header = ECHO_REPL;
+
+	messenger_send(remote_addr, remote_port, &echo_rep, sizeof(echo_rep));
+}
 
 
 PROCESS_THREAD(test_bcast_cb, ev, data)
@@ -24,6 +44,10 @@ PROCESS_THREAD(test_bcast_cb, ev, data)
     bcast_init(0);
 
     bcast_add_observer(&test_bcast_cb);
+
+    message_init( );
+
+    messenger_add_handler(ECHO_REQ, sizeof(echo_t), sizeof(echo_t), echo_handler);
 
     printf("Broadcast CB started\n");
     while(1)
