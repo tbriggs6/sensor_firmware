dfe635791681543831334c9384fc21e80a31293f
Dev <dg0932@cs.ship.edu>
2018-02-12 12:58:31 -0800

    Modified the Makefile to grab the noncoresec files from Coktiki and added a test configuration to the project-config file

--- testSensor/project-conf.h
+++ testSensor/project-conf.h
@@ -30,4 +30,21 @@
 #define NBR_TABLE_CONF_MAX_NEIGHBORS  10
 #define UIP_CONF_MAX_ROUTES           10
 
+//TODO: Test code for enabling the encryption
+//#undef LLSEC802154_CONF_ENABLED
+//#define LLSEC802154_CONF_ENABLED          1
+//#undef NETSTACK_CONF_FRAMER
+//#define NETSTACK_CONF_FRAMER              noncoresec_framer
+//#undef NETSTACK_CONF_LLSEC
+//#define NETSTACK_CONF_LLSEC               noncoresec_driver //doesont exist
+//#undef NONCORESEC_CONF_SEC_LVL
+//#define NONCORESEC_CONF_SEC_LVL           5
+//
+//#define NONCORESEC_CONF_KEY { 0x00 , 0x01 , 0x02 , 0x03 ,
+//                              0x04 , 0x05 , 0x06 , 0x07 ,
+//                              0x08 , 0x09 , 0x0A , 0x0B ,
+//                              0x0C , 0x0D , 0x0E , 0x0F }
+
+//End Encryption test code
+
 #endif /* PROJECT_CONF_H_ */
