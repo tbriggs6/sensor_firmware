721bf71856cec6fd3f5cf267a83cad9ce7e66b2e
Michael Foreman <mf9398@cs.ship.edu>
2018-02-27 19:27:27 -0500

    Removed intermediary code from testsensor.c as it has been moved into config.c

--- testSensor/testsensor.c
+++ testSensor/testsensor.c
@@ -10,11 +10,6 @@
 #include <bcast-service.h>
 #include <net/ip/uip-debug.h>
 #include <net/ipv6/uip-ds6.h>
-#include <driverlib/vims.h>
-#include <driverlib/flash.h>
-
-#include "dev/cc26xx-uart.h"
-#include "dev/serial-line.h"
 
 #include <message-service.h>
 #include "commandhandler.h"
@@ -35,54 +30,6 @@ typedef struct {
 	uint32_t header;
 	char message[32];
 } echo_t;
-
-
-extern void * _uflash_base;
-extern void * _uflash_size;
-
-/*
-void check_nvflash(){
-	config_t * currentConfig = (config_t *) &_uflash_base;
-
-	printf("Entered Check_nvFlash()\r\n");
-
-	if(currentConfig->magic != CONFIG_MAGIC)
-	{
-		uint32_t mybase = (uint32_t) &_uflash_base;
-		uint32_t mysize = (uint32_t) &_uflash_size;
-
-		config_t myconf = { .magic = CONFIG_MAGIC, .bcast_interval = 100000, .neighbor_interval = 100000, .sensor_interval = 1000};
-
-
-		FlashPowerModeSet (FLASH_PWR_ACTIVE_MODE, 1024 * 1024, 1024 * 1024);
-		uint32_t sector_size = FlashSectorSizeGet ( );
-
-		// Disable and flush the cache so that the old memory isn't there
-		VIMSModeSet(VIMS_BASE, VIMS_MODE_DISABLED);
-		while(VIMSModeGet(VIMS_BASE) != VIMS_MODE_DISABLED);
-
-		// Disable all interrupts when accessing flash, prevents memory from getting hosed
-		CPUcpsid();
-
-		// Erase the flash -- required in order to write
-		uint32_t resp = FlashSectorErase( mybase );
-		resp = FlashProgram((uint8_t *) &myconf, mybase, sizeof(config_t));
-
-		// Reenable interupts
-		CPUcpsie();
-
-		// Re-enable the cache
-		VIMSModeSet(VIMS_BASE, VIMS_MODE_ENABLED);
-		printf("New Magic: %X\r\n", currentConfig->magic);
-	}
-	else
-	{
-		printf("Current Magic: %X\r\n", currentConfig->magic);
-	}
-
-
-}
-*/
 
 void echo_handler(uip_ipaddr_t *remote_addr, int remote_port, char *data, int length)
 {
@@ -124,13 +71,6 @@ PROCESS_THREAD(test_bcast_cb, ev, data)
     messenger_add_handler(CMD_SET_HEADER, sizeof(uint32_t) * 4, sizeof(command_set_t), command_handler);
     printf("Before data handler init()\n");
     datahandler_init( );
-
-//    /* FLASH */
-//    printf("Calling check_nvflash() ... \n");
-//    check_nvflash();
-//
-//    /* end Flash */
-
 
     printf("Broadcast CB started\n");
     while(1)
