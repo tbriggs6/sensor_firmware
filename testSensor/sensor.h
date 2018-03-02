c0c4836fc8b356a252ce0687f0aa615661bf5074
Michael Foreman <mf9398@cs.ship.edu>
2018-03-02 15:46:43 -0500

    Moved more files over from version2-nocomms branch

---
+++ testSensor/sensor.h
@@ -0,0 +1,17 @@
+/*
+ * sensor.h
+ *
+ *  Created on: Jul 5, 2016
+ *      Author: user
+ */
+
+#ifndef SENSORNODE_SENSOR_H_
+#define SENSORNODE_SENSOR_H_
+
+
+#define DEVICE_TYPE (200)
+
+void sensors_send( );
+void sensor_reset_timer( );
+
+#endif /* SENSORNODE_SENSOR_H_ */
