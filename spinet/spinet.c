a032183ab00c20eecd9f0c72a4d1d93f739ca5e5
Tom Briggs <tbriggs@cs.ship.edu>
2018-11-10 17:48:55 -0500

    Its working!

--- spinet/spinet.c
+++ spinet/spinet.c
@@ -47,7 +47,7 @@
 #include "net/routing/routing.h"
 #include "net/ipv6/uip.h"
 #include "net/ipv6/uiplib.h"
-
+#include <sys/etimer.h>
 
 
 #define LOG_MODULE "SPINet"
@@ -82,7 +82,7 @@ void toggle_interrupt( )
 {
   // raise interrupt
     ti_lib_gpio_set_dio(27);
-    clock_delay_usec(50);
+    clock_delay_usec(10);
     ti_lib_gpio_clear_dio(27);
 }
 
@@ -112,7 +112,11 @@ static void dma_xfer(void *slv_out, void *slv_in, int len)
       return;
   }
 
-  printf("Starting DMA XFER\n");
+  char *cptr1 = slv_out;
+  char *cptr2 = slv_in;
+  printf("Starting DMA XFER %-2.2x%-2.2x%-2.2x%-2.2x  %-2.2x%-2.2x%-2.2x%-2.2x\n",
+	 cptr1[0],cptr1[1],cptr1[2],cptr1[3],
+	 cptr2[0],cptr2[1],cptr2[2],cptr2[3]);
   dma_xfer_inprogress = 1;
 
   uDMAChannelTransferSet(UDMA0_BASE,
@@ -120,7 +124,6 @@ static void dma_xfer(void *slv_out, void *slv_in, int len)
         		 UDMA_MODE_BASIC,
 			 (void *) (SSI0_BASE | SSI_O_DR),
         		 slv_in, len);
-
   uDMAChannelTransferSet (UDMA0_BASE,
 			  UDMA_CHAN_SSI0_TX | UDMA_PRI_SELECT,
 			  UDMA_MODE_BASIC,
@@ -202,7 +205,7 @@ uint32_t read_register(uint32_t reg)
     return regs[reg];
 
   else if (reg == 33)
-    return 0x1b15;
+    return 0x01234567;
 
   if (reg == 34)
     return 0xb00b;
@@ -237,33 +240,57 @@ static char packet_in[UIP_BUFSIZE];
 static char packet_out[UIP_BUFSIZE];
 static uint32_t rcvd_len = 0;
 
+
+///* Declare and auto-start this file's process */
+//PROCESS(intject, "INtJECT");
+//
+//PROCESS_THREAD(intject, ev, data)
+//{
+//  static struct etimer timer;
+//
+//
+//  PROCESS_BEGIN();
+//  etimer_set(&timer, 10 * CLOCK_SECOND);
+//  while(1) {
+//      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+//      printf("Toggling interrupt\n");
+//      toggle_interrupt();
+//      etimer_reset(&timer);
+//  }
+//  PROCESS_END();
+//}
+
 /*---------------------------------------------------------------------------*/
 /* Declare and auto-start this file's process */
 PROCESS(spislv_ctrl, "SPI Net");
 AUTOSTART_PROCESSES(&spislv_ctrl);
-
-
-
-
 PROCESS_THREAD(spislv_ctrl, ev, data)
 {
-  PROCESS_BEGIN();
+
   static uint32_t curr_reg = 0;
+
+  PROCESS_BEGIN();
+
+//  process_start(&intject, NULL);
 
   NETSTACK_MAC.off();
 
   LOG_INFO("\r\n\r\n\r\nContiki-NG SPI NET Starting\n");
+  LOG_INFO("Compile date %s %s", __DATE__, __TIME__);
   spi_init ();
   dma_init( );
 
-  printf ("Starting network routing\r\n");
-
+  /*/\/\/\/ network routing /\/\/\/ */
   set_prefix( );
 
   NETSTACK_MAC.on();
+
+
+  /*/\/\/\/ interrupt pin management /\/\/\ */
   ti_lib_gpio_clear_dio(27);
   GPIO_setOutputEnableDio(27, GPIO_OUTPUT_ENABLE);
 
+  //
   memset((void *)slv_out, 0xff, 4);
   memset((void *)slv_in, 0x00, 4);
   slv_state = 0;
@@ -274,7 +301,15 @@ PROCESS_THREAD(spislv_ctrl, ev, data)
   while (1)
     {
 
-      PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+      PROCESS_WAIT_EVENT();
+
+//      if (etimer_expired(&int_timer)) {
+//	  printf("Toggling interrupt\n");
+//	  toggle_interrupt();
+//	  etimer_reset(&int_timer);
+//	  continue;
+//      }
+
       printf("Rcvd Event - slave state %d\n",slv_state);
 
       // begin a command
@@ -302,13 +337,16 @@ PROCESS_THREAD(spislv_ctrl, ev, data)
 	  }
 
 	  else if (slv_in[0] == 0x20) {
-	      uint32_t len = slv_in[1] << 8 | slv_in[0];
+	      printf("%-2.2x %-2.2x %-2.2x %-2.2x",
+		     slv_in[0], slv_in[1], slv_in[2], slv_in[3]);
+
+	      uint32_t len = slv_in[1] << 8 | slv_in[2];
 	      if (len > UIP_BUFSIZE) {
 		  printf("Error - packet length exceeded\n");
 		  slv_state = 0;
 	      }
 	      else {
-		  printf("Recv packet from master %d bytes", (int) len);
+		  printf("Recv packet from master %d bytes\n", (int) len);
 		  rcvd_len = len;
 		  dma_xfer(&packet_out, &packet_in, len);
 		  slv_state = 3;
@@ -316,14 +354,19 @@ PROCESS_THREAD(spislv_ctrl, ev, data)
 	  }
 
 	  else if (slv_in[0] == 0x21) {
-	      uint32_t len = slv_in[1] << 8 | slv_in[0];
-	      if (len > 64) {
-		  printf("Error - packet length exceeded\n");
+	      printf("%-2.2x %-2.2x %-2.2x %-2.2x",
+	      		 slv_in[0], slv_in[1], slv_in[2], slv_in[3]);
+
+	      uint32_t len = slv_in[1] << 8 | slv_in[2];
+
+	      if (len > UIP_BUFSIZE) {
+		  printf("Error - packet length exceeded %d\n", (int) len);
 		  slv_state = 0;
 	      }
 	      else {
 		  printf("Sending packet to master %d bytes", (int) len);
-		  memcpy(packet_out, uip_buffer, uip_buff_len);
+		  memcpy(&packet_out, uip_buffer, len);
+		  rcvd_len = len;
 		  dma_xfer(&packet_out, &packet_in, len);
 		  slv_state = 4;
 	      }
@@ -388,27 +431,34 @@ void SSI0IntHandler(void)
 
   SSIIntClear(SSI0_BASE, flags);
 
-  // get the current DMA for the RX channel
-  mode = uDMAChannelModeGet(UDMA0_BASE, UDMA_CHAN_SSI0_RX | UDMA_PRI_SELECT);
-
-  // we've received what we expected, notify parent process
-  if(mode == UDMA_MODE_STOP)
-  {
-      SSIDMADisable(SSI0_BASE,SSI_DMA_RX);
-      uDMAChannelDisable(UDMA0_BASE, UDMA_CHAN_SSI0_RX);
-
-      dma_xfer_inprogress = 0;
-      ti_lib_int_disable(INT_SSI0_COMB );
-
-      // notify slave controller process
+  // there was a timeout - the last command aborted....
+  if (flags == SSI_RXTO) {
+      slv_state = 0;
       process_poll (&spislv_ctrl);
   }
   else {
-      SSIDMADisable(SSI0_BASE,SSI_DMA_TX);
-      uDMAChannelDisable(UDMA0_BASE, UDMA_CHAN_SSI0_TX);
-  }
+      // get the current DMA for the RX channel
+      mode = uDMAChannelModeGet(UDMA0_BASE, UDMA_CHAN_SSI0_RX | UDMA_PRI_SELECT);
 
-  uDMAIntClear(UDMA0_BASE, 0xffffffff);
+      // we've received what we expected, notify parent process
+      if(mode == UDMA_MODE_STOP)
+      {
+	  SSIDMADisable(SSI0_BASE,SSI_DMA_RX);
+	  uDMAChannelDisable(UDMA0_BASE, UDMA_CHAN_SSI0_RX);
+
+	  dma_xfer_inprogress = 0;
+	  ti_lib_int_disable(INT_SSI0_COMB );
+
+	  // notify slave controller process
+	  process_poll (&spislv_ctrl);
+      }
+      else {
+	  SSIDMADisable(SSI0_BASE,SSI_DMA_TX);
+	  uDMAChannelDisable(UDMA0_BASE, UDMA_CHAN_SSI0_TX);
+      }
+
+      uDMAIntClear(UDMA0_BASE, 0xffffffff);
+  }
 
 }
 
@@ -420,8 +470,17 @@ void rpl_init(void)
 
 int rpl_output(void)
 {
-  printf("Received packet from RF\n");
+  printf("Received packet from RF %d : \n", uip_len);
+  for (int i = 0; i < uip_len; i++) {
+      if ((i > 0) && ((i % 16) == 0)) printf("\n");
+      printf(" %-2.2x ", uip_buf[i]);
+
+  }
+  printf("\n");
+
+
   regs[1] = uip_len;
+
   uip_buff_len = uip_len;
   memcpy(uip_buffer, &uip_buf, uip_len);
 
